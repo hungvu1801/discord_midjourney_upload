0cdced301fb980d75ad24c70449ec59d0d0d5453
hungvt <vutienhung2212@gmail.com>
2023-08-26 17:14:19 +0700

    Modify run.py

--- linux/run.py
+++ linux/run.py
@@ -1,7 +1,7 @@
 from app.main import main
 import logging
 import sys
-
+from logging.handlers import TimedRotatingFileHandler
 
 logging.basicConfig(
 	filename="mainlog.log",
@@ -9,5 +9,6 @@ logging.basicConfig(
 	format="%(asctime)s: %(levelname)s : %(message)s ")
 
 if __name__ == "__main__":
+	logging.info(f"{'#'*100}")
 	logging.info("Start the program.")
 	main()
